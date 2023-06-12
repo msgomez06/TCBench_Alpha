040976f7a74b222023dc8ddd9eb3992194f7e0fb
Milton Gomez <milton.gomez@unil.ch>
2023-06-12 15:52:29 +0200

    Implemented ibtrac reading, started work on var mask generation

--- utils/constants.py
+++ utils/constants.py
@@ -18,10 +18,36 @@ class track_cols:
     def __init__(self,
                  **kwargs):
         for key, val in kwargs.items():
-            setattr(self, 
-                    key, 
-                    val)
-        pass
+            
+            if key !='__META':
+                setattr(self, 
+                        key, 
+                        val)
+            else:
+                self.__metadata = val
+    
+    def get_dtypes(self):
+        dtype_dict = {}
+        
+        for key, dtype in self.__dict__.items():
+            if key[0] != '_':
+                dtype_dict[key] = dtype if (type(dtype) != dict and dtype!=np.datetime64) else str
+        return dtype_dict
+   
+    def get_colnames(self):
+        colnames = []
+        for key, dtype in self.__dict__.items():
+            if key[0]!='_': 
+                colnames.append(key)
+        return colnames
+   
+    def get_datetime_cols(self):
+        
+        datetime_cols = []
+        
+        for key, dtype in self.__dict__.items():
+            if dtype==np.datetime64: datetime_cols.append(key)
+        return datetime_cols
 
 # Track column data for tracks prepared by Dr. Saranya Ganesh Sudheesh
 sgs_track_cols = track_cols(# Basin Corresponts to ibtracs basin
@@ -64,6 +90,70 @@ sgs_track_cols = track_cols(# Basin Corresponts to ibtracs basin
                             # Coordinates
                             lat = np.float16,
                             lon = np.float16,
-                        )
+                        
+                            # Metadata for constructing track objects
+                            __META = {'UID':'SID',
+                                      'COSMETIC_NAME':'NAME',
+                                      'Y_coord':'lat',
+                                      'X_coord':'lon'}
+                            )
+
+# Track column data for ibtracs file
+ibtracs_cols = track_cols(# Storm ID number
+                          SID = str,
+                          
+                          # Year of the event
+                          SEASON = int,
+                          
+                          # Number of event in season CURRENTLY IGNORED
+                          # NUMBER = int
+                          
+                          # Basin
+                          BASIN = {'NA':'North Atlantic',
+                                   'EP':'Eastern North Pacific',
+                                   'WP':'Western North Pacific',
+                                   'NI':'North Indian',
+                                   'SI':'South Indian',
+                                   'SP':'South Pacific',
+                                   'SA':'South Atlantic',
+                                   'MM':'Missing' #Note that MM should not appear in final ibtracs product
+                                   },
+                            
+                          # Subbasin
+                          SUBBASIN = {'MM':'Missing',
+                                      'CS':'Caribbean Sea',
+                                      'GM':'Gulf of Mexico',
+                                      'CP':'Central Pacific',
+                                      'BB':'Bay of Bengal',
+                                      'AS':'Arabian Sea',
+                                      'WA':'Western Australia',
+                                      'EA':'Eastern Australia',
+                                      },
+                            
+                          # Name of the storm provided by the agencies
+                          NAME = str,
+                            
+                          # Track point ISO timestamp
+                          ISO_TIME = np.datetime64,
+                            
+                          # IBTRACS Combined storm type
+                          NATURE = {'DS':'Disturbance',
+                                    'TS':'Tropical',
+                                    'ET':'Extratropical',
+                                    'SS':'Subtropical',
+                                    'NR':'Not Reported',
+                                    'MX':'Mixture',
+                                    },
+                            
+                          # Coordinates
+                          LAT = np.float16,
+                          LON = np.float16,
+                          
+                          # Metadata for constructing track objects
+                          __META = {'UID':'SID',
+                                    'COSMETIC_NAME':'NAME',
+                                    'Y_coord':'LAT',
+                                    'X_coord':'LON'}
+                          )
 
 #%%
--- utils/toolbox.py
+++ utils/toolbox.py
@@ -13,6 +13,7 @@ used in other scripts to perform the tasks associated with TCBench
 
 import os
 import pandas as pd
+import numpy as np
 import constants
 
 # Retrieve Repository Path
@@ -24,8 +25,62 @@ print(repo_path)
 This cell contains the following classes and functions:
     
 """
+def read_hist_track_file(tracks_path = f'{repo_path}/tracks/ibtracs/',
+                         backend = pd.read_csv,
+                         track_cols = constants.ibtracs_cols,
+                         skip_rows = [1],
+                         ):
+    """
+    Function to read storm track files that include all the historical data
+    in a single file
+    
+    Parameters
+    ----------
+    tracks_path : STR, required
+        Path to the folder holding the storm track in a single file. 
+        The default is f'{repo_path}/tracks/ibtracs'. By default the file 
+        is expected to be in csv format
+    
+    backend : FUNC, required
+        file handler for loading the track file. The default is pandas' 
+        read_csv function.
+    
+    track_cols : track_cols object defined in constants.py, required
+        Track columns object which contains data about the columns in the 
+        tracks file. Needs to at least be able to return the column names,
+        column dtypes (excluding datetime types), and columns that should
+        be parsed as dates.
+        
+    skip_rows : list, optional
+        rows to skip when reading in data with pandas.read_csv
+        Required when working with ibtracs, as there is a unit row after
+        the header row, hence the default [1] value
+        
 
-def read_py_track_file(tracks_path = f'{repo_path}/tracks/',
+    Returns
+    -------
+    track_object : type defined by backend. Default is pandas dataframe
+
+    """
+    file_list = os.listdir(tracks_path)
+    
+    assert len(file_list) == 1, f"{tracks_path} has more than one file. Aborting."
+    
+    with open(f'{tracks_path}/{file_list[0]}') as handle:
+        if backend == pd.read_csv:      
+
+            data = backend(handle,
+                           usecols = track_cols.get_colnames(),
+                           dtype = track_cols.get_dtypes(),
+                           skiprows = skip_rows,
+                           parse_dates = track_cols.get_datetime_cols(),
+                           #index_col = track_cols._track_cols__metadata['UID'],
+                           )
+            
+    
+    return data
+
+def read_py_track_file(tracks_path = f'{repo_path}/tracks/sgstracks/',
                        year = 2000,
                        backend = pd.read_csv
                        ):
@@ -35,7 +90,7 @@ def read_py_track_file(tracks_path = f'{repo_path}/tracks/',
     Parameters
     ----------
     tracks_path : STR, required
-        Path to the location holding the track paths separated by year. 
+        Path to the folder holding the storm tracks separated by year. 
         The default is f'{repo_path}/tracks/'. By default the files are
         expected to be in csv format
     year : INT, required
@@ -46,7 +101,7 @@ def read_py_track_file(tracks_path = f'{repo_path}/tracks/',
 
     Returns
     -------
-    track_object : type defined by backend. Default is pandas 
+    track_object : type defined by backend. Default is pandas dataframe
 
     """
     # Retrieve the list of files from the path
@@ -67,10 +122,97 @@ def process_py_track_data(data,
     
     for column in track_cols.__dict__.keys():
         assert column in data.columns, f'{column} not found in header columns'
+        
+        if type(track_cols.__dict__[column])==dict: print(column)
+    
+    # TODO: 
     
     pass
 
+# Class presenting each track type
 class tc_track:
     def __init__(self,
-                 ):
+                 UID,
+                 NAME,
+                 track,
+                 timestamps):
+        assert type(UID) == str, f'Unique Identifier Error. {type(UID)} given when string was expected'
+        assert type(NAME) == str, f'Name Error. {type(NAME)} given when string was expected'
+        assert type(track) == str, f'Track Error. {type(track)} given when numpy array was expected'
+        assert (type(timestamps) == np.ndarray or type(timestamps) == pd.DataFrame), f'Unsupported timestamp iterator {type(timestamps)}'
+        assert np.issubdtype(timestamps.dtype, np.datetime64), f'Unsupported timestamp type: {timestamps.dtype} - expected datetime64'
+        
+        self.uid = UID
+        self.name = NAME
+        self.track = track,
+        self.timestamps = timestamps
+        
+    
+    def get_varmask(self,
+                    grid,
+                    distance_calculator,
+                    ):
         pass
+
+
+#%% Auxilliary Functions
+
+# Lat-lon grid generator
+def ll_gridder(origin = (0,0),
+               resolution = 0.25,
+               lon_mode = 'pos',
+               ):
+    """
+    ll_gridder is a lat-lon grid generator that's useful for generator the
+    data masks associated with the     
+    
+    Parameters
+    ----------
+    origin : TYPE, optional
+        DESCRIPTION. The default is (0,0).
+    resolution : TYPE, optional
+        DESCRIPTION. The default is 0.25.
+    lon_mode : TYPE, optional
+        DESCRIPTION. The default is 'pos'.
+     : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    pass
+
+# Great circle distance calculations
+def haversine(latp, lonp, lat_list, lon_list):
+    """──────────────────────────────────────────────────────────────────────────┐
+      Haversine formula for calculating distance between target point (latp, 
+      lonp) and series of points (lat_list, lon_list). This function can handle 
+      2D lat/lon lists, but has been used with flattened data
+      
+      Based on:
+      https://medium.com/@petehouston/calculate-distance-of-two-locations-on-earth-using-python-1501b1944d97
+      
+      
+      Inputs:
+          latp - latitude of target point
+          
+          lonp - longitude of target point
+          
+          lat_list - list of latitudess (lat_p-1, lat_p-2 ... lon_p-n)
+      
+          lon_list - list of longitudess (lon_p-1, lon_p-2 ... lon_p-n)
+      
+      Outputs:
+      
+    └──────────────────────────────────────────────────────────────────────────"""
+    latp = np.radians(latp)
+    lonp = np.radians(lonp)
+    lat_list = np.radians(lat_list)
+    lon_list = np.radians(lon_list)
+    
+    dlon = lonp - lon_list
+    dlat = latp - lat_list
+    a = np.power(np.sin(dlat / 2),2) + np.cos(lat_list) * np.cos(latp) * np.power(np.sin(dlon / 2), 2)
+    return 2 * 6371 * np.arcsin(np.sqrt(a))

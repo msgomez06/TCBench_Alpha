8832c6320926de74cc10c003407e5075c50689cd
Milton Gomez <milton.gomez@unil.ch>
2024-01-11 15:52:51 +0100

    added visualization of datastore contents

--- dev/utils/data.py
+++ dev/utils/data.py
@@ -13,6 +13,8 @@ used in other scripts to perform the data tasks associated with TCBench
 import os
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 default = "/work/FAC/FGSE/IDYST/tbeucler/default/raw_data/ECMWF/ERA5/"
 
@@ -68,7 +70,6 @@ class Data_Collection:
         var_dictionary = {}
         global_year_list = []
         for var in var_list:
-            print(var)
             file_list = sorted(os.listdir(os.path.join(var_path, var)))
 
             avail_years = []
@@ -79,8 +80,8 @@ class Data_Collection:
                 ), f"{file} is not a(n) {self.file_type} file"
                 avail_years.append(file.split(".")[0].split("_")[1])
             global_year_list += avail_years
-            var_dictionary[var] = avail_years
-        global_year_list = list(set(global_year_list))
+            var_dictionary[var] = sorted(avail_years)
+        global_year_list = sorted(list(set(global_year_list)))
 
         if hasattr(self, "meta_dfs"):
             self.meta_dfs[var_type] = self._check_availability(
@@ -90,9 +91,6 @@ class Data_Collection:
             self.meta_dfs = {
                 var_type: self._check_availability(var_dictionary, global_year_list)
             }
-        # self.meta_df = self._check_availability(var_dictionary, global_year_list)
-        # self.var_dict = var_dictionary
-        # self.gyl = sorted(global_year_list)
 
     def _check_availability(self, var_dict, all_years):
         # Initialize an empty DataFrame with the variable names as the index and the years as the columns
@@ -108,5 +106,73 @@ class Data_Collection:
 
         return df
 
+    # Function to print the availability of variables
+    def variable_availability(self, **kwargs):
+        assert hasattr(
+            self, "meta_dfs"
+        ), "The data collection object has not been properly initialized."
+
+        save_image = kwargs.get("save_image", False)
+        save_path = kwargs.get("save_path", None)
+
+        assert (save_image and save_path) or (
+            not save_image and not save_path
+        ), "If save_image is True, save_path must be specified."
+
+        for key in dc.meta_dfs.keys():
+            # Create a colormap
+            cmap = mcolors.ListedColormap(
+                ["black"]
+                + (list(plt.cm.tab20c.colors) * 10)[: len(dc.meta_dfs[key].index)]
+            )
+            norm = mcolors.Normalize(vmin=-0.5, vmax=len(dc.meta_dfs[key].index) + 0.5)
+            fig, ax = plt.subplots()
+            ax.matshow(
+                dc.meta_dfs[key].to_numpy()
+                * (np.arange(0, len(dc.meta_dfs[key].index)) + 1).reshape(-1, 1),
+                cmap=cmap,
+                norm=norm,
+            )
+            ax.set_yticks(np.arange(0, len(dc.meta_dfs[key].index)))
+            ax.set_xticks(np.arange(0, len(dc.meta_dfs[key].columns)))
+            ax.set_xticklabels(
+                dc.meta_dfs[key].columns, rotation="vertical", ha="center"
+            )
+            ax.set_yticklabels(dc.meta_dfs[key].index)
+            plt.show()
+
+            if save_image:
+                plt.savefig(save_path + key + ".png")
+
 
 # %% Functions
+
+# %% Test running the data collection class
+
+if __name__ == "__main__":
+    # Test running the data collection class
+    dc = Data_Collection(default)
+
+    # # Test the variable availability function
+    dc.variable_availability()
+
+    # for key in dc.meta_dfs.keys():
+    #     # Create a colormap
+    #     cmap = mcolors.ListedColormap(
+    #         ["black"] + (list(plt.cm.tab20c.colors) * 10)[: len(dc.meta_dfs[key].index)]
+    #     )
+    #     norm = mcolors.Normalize(vmin=-0.5, vmax=len(dc.meta_dfs[key].index) + 0.5)
+    #     fig, ax = plt.subplots()
+    #     ax.matshow(
+    #         dc.meta_dfs[key].to_numpy()
+    #         * (np.arange(0, len(dc.meta_dfs[key].index)) + 1).reshape(-1, 1),
+    #         cmap=cmap,
+    #         norm=norm,
+    #     )
+    #     ax.set_yticks(np.arange(0, len(dc.meta_dfs[key].index)))
+    #     ax.set_xticks(np.arange(0, len(dc.meta_dfs[key].columns)))
+    #     ax.set_xticklabels(dc.meta_dfs[key].columns, rotation="vertical", ha="center")
+    #     ax.set_yticklabels(dc.meta_dfs[key].index)
+    #     plt.show()
+
+# %%
